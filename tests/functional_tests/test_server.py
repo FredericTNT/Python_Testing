381c80d26a04ca111caf458016c362dbbc14c553
Frederic THOUVENOT <frederic.tnt78@gmail.com>
2022-09-14 09:59:08 +0200

    Feature Points Display Board v1.1 & Testing

--- tests/functional_tests/test_server.py
+++ tests/functional_tests/test_server.py
@@ -3,7 +3,8 @@ from selenium.webdriver.chrome.service import Service as CS
 from selenium.webdriver.firefox.service import Service as FS
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
-from time import sleep
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 
 class TestWebSiteFirefox:
@@ -11,15 +12,44 @@ class TestWebSiteFirefox:
     def setup_method(self):
         service = FS('tests/functional_tests/geckodriver')
         self.driver = webdriver.Firefox(service=service)
-
-    def test_login(self):
         self.driver.get("http://127.0.0.1:5000")
-        assert "GUDLFT" in self.driver.title
-        elem = self.driver.find_element(By.NAME, "email")
-        elem.send_keys("admin@irontemple.com")
-        elem.send_keys(Keys.RETURN)
-        sleep(5)
-        assert "<h2>Welcome, admin@irontemple.com </h2>" in self.driver.page_source
+        WebDriverWait(self.driver, timeout=5).until(EC.title_is("GUDLFT Registration"))
+        assert "Registration" in self.driver.title
+
+    def test_user_story_happy_end(self):
+        """
+        Parcours client #HappyEnd (site Flask on-line)
+            - Identification
+            - Choix de la compétition
+            - Réservation des places
+            - Affichage Dashboard
+            - Déconnexion
+        """
+        club_email = "admin@irontemple.com"
+        places_required = "1"
+
+        main_windows = self.driver.current_window_handle
+        self.driver.find_element(By.NAME, "email").send_keys(club_email + Keys.RETURN)
+        WebDriverWait(self.driver, timeout=5).until(EC.title_contains("Summary"))
+        assert "Welcome, " + club_email in self.driver.page_source
+
+        self.driver.find_elements(By.LINK_TEXT, "Book Places")[0].click()
+        WebDriverWait(self.driver, timeout=5).until(EC.title_contains("Booking"))
+        assert "Booking for" in self.driver.title
+
+        self.driver.find_element(By.NAME, "places").send_keys(places_required + Keys.RETURN)
+        WebDriverWait(self.driver, timeout=5).until(EC.title_contains("Summary"))
+        assert "Great-booking complete!" in self.driver.page_source
+
+        self.driver.find_element(By.LINK_TEXT, "Dashboard").click()
+        self.driver.switch_to.window("dashboard")
+        WebDriverWait(self.driver, timeout=5).until(EC.title_contains("Dashboard"))
+        assert "Dashboard" in self.driver.title
+
+        self.driver.switch_to.window(main_windows)
+        self.driver.find_element(By.LINK_TEXT, "Logout").click()
+        WebDriverWait(self.driver, timeout=5).until(EC.title_is("GUDLFT Registration"))
+        assert "Registration" in self.driver.title
 
     def teardown_method(self):
         self.driver.quit()
@@ -30,16 +60,60 @@ class TestWebSiteChrome:
     def setup_method(self):
         service = CS('tests/functional_tests/chromedriver')
         self.driver = webdriver.Chrome(service=service)
-
-    def test_login(self):
         self.driver.get("http://127.0.0.1:5000")
-        assert "GUDLFT" in self.driver.title
-        elem = self.driver.find_element(By.NAME, "email")
-        elem.send_keys("admin@irontemple.com")
-        elem.send_keys(Keys.RETURN)
-        sleep(5)
-        assert "<h2>Welcome, admin@irontemple.com </h2>" in self.driver.page_source
+        WebDriverWait(self.driver, timeout=5).until(EC.title_is("GUDLFT Registration"))
+        assert "Registration" in self.driver.title
+
+    def test_user_story_bad_trip(self):
+        """
+        Parcours client #BadTrip (site Flask on-line)
+            - Identification (échec : adresse mail inexistante)
+            - Identification
+            - Choix de la compétition (échec : date de la compétition dépassée)
+            - Choix de la compétition
+            - Réservation des places (échec : nombre négatif de places)
+            - Réservation des places (échec : pas assez de points)
+            - Affichage Dashboard
+            - Déconnexion
+        """
+        club_email = "john@simplylift.co"
+
+        main_windows = self.driver.current_window_handle
+        self.driver.find_element(By.NAME, "email").send_keys("wrong@email" + Keys.RETURN)
+        WebDriverWait(self.driver, timeout=5).until(EC.title_is("GUDLFT Registration"))
+        assert "Wrong email, please try again" in self.driver.page_source
+
+        self.driver.find_element(By.NAME, "email").send_keys(club_email + Keys.RETURN)
+        WebDriverWait(self.driver, timeout=5).until(EC.title_contains("Summary"))
+        assert "Welcome, " + club_email in self.driver.page_source
+
+        self.driver.find_elements(By.LINK_TEXT, "Book Places")[1].click()
+        WebDriverWait(self.driver, timeout=5).until(EC.title_contains("Summary"))
+        assert "You cannot book a past competition" in self.driver.page_source
+
+        self.driver.find_elements(By.LINK_TEXT, "Book Places")[0].click()
+        WebDriverWait(self.driver, timeout=5).until(EC.title_contains("Booking"))
+        assert "Booking for" in self.driver.title
+
+        places_required = "-2"
+        self.driver.find_element(By.NAME, "places").send_keys(places_required + Keys.RETURN)
+        WebDriverWait(self.driver, timeout=5).until(EC.title_contains("Booking"))
+        assert "You need to book a positive integer of places" in self.driver.page_source
+
+        places_required = "20"
+        self.driver.find_element(By.NAME, "places").send_keys(places_required + Keys.RETURN)
+        WebDriverWait(self.driver, timeout=5).until(EC.title_contains("Summary"))
+        assert "Not enough points to book so many places" in self.driver.page_source
+
+        self.driver.find_element(By.LINK_TEXT, "Dashboard").click()
+        self.driver.switch_to.window("dashboard")
+        WebDriverWait(self.driver, timeout=5).until(EC.title_contains("Dashboard"))
+        assert "Dashboard" in self.driver.title
+
+        self.driver.switch_to.window(main_windows)
+        self.driver.find_element(By.LINK_TEXT, "Logout").click()
+        WebDriverWait(self.driver, timeout=5).until(EC.title_is("GUDLFT Registration"))
+        assert "Registration" in self.driver.title
 
     def teardown_method(self):
         self.driver.quit()
-
--- tests/integration_tests/test_server.py
+++ tests/integration_tests/test_server.py
@@ -16,21 +16,25 @@ def client():
 def test_user_story_1(client):
     """
     Parcours client #1 (mode client Flask off-line)
-        - Identification via adresse email
-        - Choix de la compétition
+        - Identification
+        - Choix de la compétition (échec : date de la compétition dépassée)
         - Réservation des places
-        > Confirmation de la réservation
+        - Affichage Dashboard
     """
-    competition_name = "Spring Festival"
+    competition_name = "Fall Classic"
     club_name = "Simply Lift"
     club_email = "john@simplylift.co"
     places = 3
     response = client.post('/showSummary', data={'email': club_email})
     assert response.status_code == 200
     response = client.get('/book/' + competition_name + '/' + club_name)
+    data = response.data.decode()
     assert response.status_code == 200
+    assert data.find("You cannot book a past competition") != -1
     response = client.post('/purchasePlaces', data={
         'competition': competition_name, 'club': club_name, 'places': places})
     data = response.data.decode()
     assert response.status_code == 200
     assert data.find("Great-booking complete!") != -1
+    response = client.get('/pointsDisplay')
+    assert response.status_code == 200
